d35236d66d74add415fa3d7320f333ab98cba1d5
q1nyy <1049860975@qq.com>
2023-11-29 01:58:25 -0800

    modifiedDataSet

--- src/dp.cpp
+++ src/dp.cpp
@@ -29,7 +29,7 @@ int main() {
 
     //状态转移
     for(int i = 0; i < n; i ++) {
-        for(int j = m; j > w[i]; j --) {
+        for(int j = m; j >= w[i]; j --) {
             dp[j] = max(dp[j], dp[j - w[i]] + v[i]);
         }
     }
