7be506ded9289e29574b3edfb36ec11901a285c0
q1nyy <1049860975@qq.com>
2023-12-18 12:47:40 +0800

    modify brachBound

--- src/brachBound.cpp
+++ src/brachBound.cpp
@@ -9,6 +9,7 @@ struct Item {
 struct Node {
     int ne, totW, totV;
     double upV;
+    vector<int> items; //用于存放选了哪些物品
 
     bool operator < (const Node &T) const {
         return upV < T.upV;
@@ -30,7 +31,8 @@ int main() {
     ranges::sort(g, [](const Item &a, const Item &b) {
         return 1.0 * a.v / a.w > 1.0 * b.v / b.w;
     });
-
+    
+    //计算上界
     auto bound = [&](int x, int curW, int curV) {
         int Wleft = m - curW;
         double upv = curV;
@@ -49,39 +51,62 @@ int main() {
     };
 
     auto bfs = [&] {
-        Node r = {0, 0, 0,bound(0, 0, 0)};
+        Node r = {0, 0, 0, bound(0, 0, 0), {}};
         priority_queue<Node> q;
         q.push(r);
 
+        Node bestNode;  
         int ans = 0;
         while(q.top().upV > ans) {
             auto t = q.top();
             q.pop();
 
             if(t.ne == n) {
-                ans = max(ans, t.totV);
+                if(t.totV > ans) {
+                    ans = t.totV;
+                    bestNode = t; 
+                }
             } else {
                 Node t2 = t;
                 if(t.totW + g[t.ne].w <= m) {
-                    t.upV = bound(t.ne, t.totW, t.totV);
+                    t.upV = bound(t.ne, t.totW, t.totV);//选取当前物品
                     if(t.upV > ans) {
                         t.totV += g[t.ne].v;
                         t.totW += g[t.ne].w;
+                        t.items.push_back(g[t.ne].id);  
                         t.ne ++;
                         q.push(t);
                     }
                 }
-                t2.upV = (t2.ne + 1, t2.totW, t2.totV);
+                t2.upV = (t2.ne + 1, t2.totW, t2.totV);//不选当前物品
                 if(t.upV > ans ) {
                     t2.ne ++;
                     q.push(t2);
                 }
             }
         }
-        return ans;
+
+        cout << "分支限界解决01背包问题\n\n";
+        cout << "物品个数：" << n << ' ' << "背包容量：" << m << "\n\n";
+
+        for(int i = 0; i < n; i ++) {
+            cout << "物品" << i + 1 << "的体积价值：" << g[i].w << ' ' << g[i].v << '\n';
+        }
+        cout << '\n';
+
+        vector<bool> selected(n);
+        for(int id : bestNode.items) {
+            selected[id] = true;
+        }
+
+        cout << "最大价值为：" << ans << '\n';
+        
+        cout << "解向量为：";
+        for(auto c : selected) cout << c << ' ';
+        cout << '\n';
     };
 
-    cout << bfs() << '\n';
+    bfs();
 
     return 0;
 }
